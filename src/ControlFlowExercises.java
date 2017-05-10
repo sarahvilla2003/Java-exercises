ffcdfa6c1f99efd1dcd418367e2df713237c3f95
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-10 14:13:51 -0500

    Do/While loop: questions completed fully

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -24,13 +24,42 @@ public class ControlFlowExercises {
 //            i+=2;
 //        } while (i<=100);
 
+        // refactor previous do while loop into for loop
+//        for (int i=0; i<=100; i+=2){
+//            System.out.println(i);
+//        }
 
-        int i =100;
-        do {
-            System.out.println(i);
-            i -=5;
-        } while ( i>=-10);
+//
+//        int i =100;
+//        do {
+//            System.out.println(i);
+//            i -=5;
+//        } while ( i>=-10);
 
+        //Refactor
+
+//        for (int i=100; i>=-10; i-=5){
+//            System.out.println(i);
+//        }
+
+//
+//        int i=1000;
+//        do {
+//            System.out.println(i);
+//            i/=10;
+//        } while (i>=1);
+
+//        int i=200000000;
+//        do {
+//            System.out.println(i);
+//            i= (int)Math.pow(i,2);
+//        } while (i <=1000000);
+
+        //Refactor
+
+//        for (int i=200000000; i<=1000000; i=(int)Math.pow(i,2)){
+//            System.out.println(i);
+//        }
 //      ----- FizzBuzz Test ------------------------------------------------
 //
 //        int i = 1;
