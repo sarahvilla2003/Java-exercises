673dab151567f30606ca5c76ebea1b8c38747ded
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-10 13:22:02 -0500

    Do/While loop: Count by 5 from 100 to -10

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,30 +8,28 @@ public class ControlFlowExercises {
         Scanner scan = new Scanner(System.in);
 
 //      -----  While Loop  ------------------------------------------------
-
-        int i = 5;
-        System.out.println(i);
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//
+//        int i = 5;
+//        System.out.println(i);
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
 //      -----  Do While Loop ----------------------------------------------
-
+        // Increments by 2
 //        int i= 0;
 //        do {
 //            System.out.println(i);
-//            i<=100;                                           i +=2;
-//            i++;                                              no
-//        } while ( i %2 !=0);                                  (i <=100);
-//        continue;
-//        System.out.println(i);
+//            i+=2;
+//        } while (i<=100);
 
-//        int i = -10;
-//        do {
-//            System.out.println(i);
-//            i +=5;
-//        }
+
+        int i =100;
+        do {
+            System.out.println(i);
+            i -=5;
+        } while ( i>=-10);
 
 //      ----- FizzBuzz Test ------------------------------------------------
 //
