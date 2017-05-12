8fea561b725e87affab643fd82950a529af1bebb
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-12 09:38:47 -0500

    Strings Exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,21 @@
+/**
+ * Created by SarahVillareal on 5/11/17.
+ */
+public class StringExercise {
+    public static void main(String[] args) {
+        String message;
+        message ="We don't need no education";
+        System.out.println(message);
+        message = "We don't need no thought control";
+        System.out.println(message);
+        message = "Check \"this\" out!, \"s inside of \" s!";
+        System.out.println(message);
+        message = "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+        message = "I can do backslashes \\, double backslashes \\\\, \n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+
+
+    }
+}
