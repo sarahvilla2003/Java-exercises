65941772e9e9ae8aa5b280544b1375169dc9f2c4
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-15 11:32:23 -0500

    Methods: Factorial Exercise, user input added

--- src/BasicArithmeticExercise.java
+++ src/BasicArithmeticExercise.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by SarahVillareal on 5/12/17.
  */
 public class BasicArithmeticExercise {
     public static void main(String[] args) {
-    long number =4;
-    long factorial =1;
-    for (int i =1; i<=number; i++){
-        factorial = factorial * i;
-    }
-        System.out.println(number + "!=" + factorial);
+        long number;
+        long factorial;
+        Scanner input = new Scanner(System.in);
+
+        String wantsToContinue;
+        do {
+
+            System.out.println("Enter an integer number");
+            number = input.nextLong();
+
+            factorial = 1;
+            for (int i = 1; i <= number; i++) {
+                factorial = factorial * i;
+            }
+            System.out.println(number + "!=" + factorial);
+            System.out.println("Do you want to continue? (y/n)");
+            input.nextLine();
+            wantsToContinue =input.nextLine();
+        } while ("y".equalsIgnoreCase(wantsToContinue));
     }
 }
