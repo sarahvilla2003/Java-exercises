12d0fef7e2abad8ba4f6aa97092b189662929116
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-17 17:00:34 -0500

    Object Java Exercise #3 more input

---
+++ src/Java2/InputTest.java
@@ -0,0 +1,18 @@
+package Java2;
+
+/**
+ * Created by SarahVillareal on 5/17/17.
+ */
+public class InputTest {
+    public static void main(String[] args) {
+//        Input entry = new Input();
+//        System.out.println("Do you love school?");
+//        System.out.println(entry.yesNo());
+//        int min = 1;
+//        int max = 10;
+//
+//        System.out.println("Type a number between "+ min +"and" + max);
+//        System.out.println("The number in the range was: " + entry.getInt(min, max));
+
+    }
+}
