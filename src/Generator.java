12ac7a4684a1dda18854681015173e333687621b
SarahVillareal <sarahvilla2003@yahoo.com>
2017-05-18 15:37:34 -0500

    Arrays Java Exercise #1 Server Name Generator: made arrays

---
+++ src/Generator.java
@@ -0,0 +1,15 @@
+/**
+ * Created by SarahVillareal on 5/18/17.
+ */
+public class Generator {
+    public static void main(String[] args) {
+//       String adjArray = new String() [10];
+//        String nounArray= new String[10];
+        String[] adj = {"friendly", "beautiful", "smart", "kind", "gentle",
+                "tough", "rugged", "strong", "handsome", "active"};
+        String[] noun = {"Sarah", "Susie", "Jessica", "Amanda", "Heather",
+                "John", "Adam", "Ryan", "Jo", "Steven"};
+    }
+    
+}
+
